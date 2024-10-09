cbb114215703164365bee248b25cba9b65871643
Will Ramos <ramoswillyam1@gmail.com>
2024-10-09 14:27:22 -0700

    Initial branch commit

--- src/App.js
+++ src/App.js
@@ -3,30 +3,20 @@ import React, { useEffect, useState } from 'react';
 import axiosConfig from './api/axiosConfig';
 
 function App() {
-  const [cars, setCars] = useState([]);
+  // const [cars, setCars] = useState([]);
 
-  const fetch = async () => {
-    const response = await axiosConfig.get("/api/v1/cars");
-    setCars(response.data);
-    console.log(response)
-  }
+  // const fetch = async () => {
+  //   const response = await axiosConfig.get("/api/v1/cars");
+  //   setCars(response.data);
+  //   console.log(response)
+  // }
 
-  useEffect(() => {
-    fetch();
-  }, [])
+  // useEffect(() => {
+  //   fetch();
+  // }, [])
   return (
     <div className="App">
-      {cars.map((i, idx) => {
-        return (
-          <div key={idx}>
-            <h1>{i.year}</h1>
-            {i.make}
-            {i.reviewIds.length ? i.reviewIds.map((i) => {
-              return <li>{i.body}</li>
-            }) : null}
-          </div>
-        )
-      })}
+      hi
     </div>
   );
 }
