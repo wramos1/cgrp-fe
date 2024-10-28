64fe1e7c1f7a1119cb1fa1a0dd1ac9dd1a01546e
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2024-10-28 14:22:40 -0700

    login html

--- src/login-page/login-page.js
+++ src/login-page/login-page.js
@@ -1,58 +1,21 @@
 import './login-page.css';
-import React, { useState } from 'react';
-import loginBackground from '../images/login.png'
-import { Link } from 'react-router-dom';
-import axiosConfig from '../api/axiosConfig';
+import React from 'react';
+import login from '../images/login.png'
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const loginUser = async (e) => {
-    e.preventDefault();
-    if (username.trim() === '' || password.trim() === '') {
-      alert("Username is required");
-      return;
-    }
-
-    const formData = new URLSearchParams();
-    formData.append('username', username);
-    formData.append('password', password);
-
-
-    const result = await axiosConfig.post('/login', formData);
-
-
-    setUsername('');
-    setPassword('');
-
-    console.log(result)
-  }
-
   return (
     <div>
       <div>
-        <img src={loginBackground} className="login-background" />
+        <img src={login} className="login-background" />
         <div className="bluebox">
           <h2 className="login-text">LOG IN</h2>
-          <form className="submit-form" onSubmit={(e) => loginUser(e)}>
-            <label for="username">
-              Username:
-            </label>
-            <br />
-            <input required value={username} type="text" id="username" name="username" onChange={(e) => setUsername(e.target.value)} />
-            <br />
-            <label for="pwd">
-              Password:
-            </label>
-            <br />
-            <input required value={password} id="pwd" name="password" onChange={(e) => setPassword(e.target.value)} />
-            <br />
-            <p id='account'>
-              No account?
-              <Link to={'/signup'} >Create one!</Link>
-            </p>
-            <input type="submit" value="LOG IN" className="button" />
+          <form className="submit-form" action="http://localhost:8080/login" method="POST">
+            <label for="username">Email:</label><br/>
+            <input type="text" id="username" name="username"/><br/>
+            <label for="pwd">Password:</label><br/>
+            <input type="password" id="pwd" name="password" /><br/>
+            <label for="account">No account? Create one!</label><br/><br/>
+            <input type="submit" value="LOG IN" className="button"/>
           </form>
         </div>
       </div>
