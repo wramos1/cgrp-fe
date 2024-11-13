9e4888dfc88bdb81e707fd3a40d8a0b3ba4707b5
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2024-11-13 14:01:30 -0800

    test commit

--- src/vehicle-page/vehicle-page-body.js
+++ src/vehicle-page/vehicle-page-body.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './vehicle-page.css';
 import truck from '../images/truck.png';
 import SUV from '../images/SUV.png';
@@ -6,40 +7,58 @@ import sedan from '../images/sedan.png';
 import coupe from '../images/coupe.png';
 import luxury from '../images/luxury.png';
 import classic from '../images/classic.png';
+import VehicleList from '../components/VehicleList';
+import axiosConfig from '../api/axiosConfig';
 
 
-const Body = () => {
+const Body = ({}) => {
+  const [vehicles, setVehicles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const searchByType = async(vehicleType) => {
+    try {
+      const response = await axiosConfig.get(`/home/typeSearch/${vehicleType}`);
+      setVehicles(response.data);
+  } catch (error) {
+      console.error("Error fetching vehicles:", error);
+  } finally {
+      setLoading(false);
+  }
+  };
+
   return (
-    <div className="card-container">
-      <div className="card-box">
-        <img src={truck} alt="Truck" />
-        <h2>Truck</h2>
-        <button>View Now</button>
-      </div>
-      <div className="card-box">
-        <img src={SUV} alt="Truck" />
-        <h2>SUV</h2>
-        <button>View Now</button>
-      </div>
-      <div className="card-box">
-        <img src={sedan} alt="Truck" />
-        <h2>Sedan</h2>
-        <button>View Now</button>
-      </div>
-      <div className="card-box">
-        <img src={coupe} alt="Truck" />
-        <h2>Coupe</h2>
-        <button>View Now</button>
-      </div>
-      <div className="card-box">
-        <img src={luxury} alt="Truck" />
-        <h2>Luxury</h2>
-        <button>View Now</button>
-      </div>
-      <div className="card-box">
-        <img src={classic} alt="Truck" />
-        <h2>Classic</h2>
-        <button>View Now</button>
+    <div>
+      <div className="card-container">
+        <div className="card-box">
+          <img src={truck} alt="Truck" />
+          <h2>Truck</h2>
+          <button onClick={() => searchByType('Truck')}>View Now</button>
+        </div>
+        <div className="card-box">
+          <img src={SUV} alt="SUV" />
+          <h2>SUV</h2>
+          <button onClick={() => searchByType('SUV')}>View Now</button>
+        </div>
+        <div className="card-box">
+          <img src={sedan} alt="Sedan" />
+          <h2>Sedan</h2>
+          <button onClick={() => searchByType('Sedan')}>View Now</button>
+        </div>
+        <div className="card-box">
+          <img src={coupe} alt="Coupe" />
+          <h2>Coupe</h2>
+          <button onClick={() => searchByType('Coupe')}>View Now</button>
+        </div>
+        <div className="card-box">
+          <img src={luxury} alt="Luxury" />
+          <h2>Luxury</h2>
+          <button onClick={() => searchByType('Luxury')}>View Now</button>
+        </div>
+        <div className="card-box">
+          <img src={classic} alt="Classic" />
+          <h2>Classic</h2>
+          <button onClick={() => searchByType('Classic')}>View Now</button>
+        </div>
       </div>
     </div>
   );
